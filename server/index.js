f290bae99a123db783930100671cce85505245a2
harshvardhan khairnar <harshvardhan@kali.kali.org>
2023-10-26 12:01:00 +0530

    signup api & login api create

--- server/index.js
+++ server/index.js
@@ -18,7 +18,52 @@ const connectDb = () => {
 
 const User = model("User", userSchema);
 
+app.post("/signup", async (req, res) => {
+  try {
+    const { name, password, email, mobile, address, gender } = req.body;
+    const newUser = new User({
+      name,
+      password,
+      email,
+      mobile,
+      address,
+      gender,
+    });
 
+    const saveUser = await newUser.save();
+    return res.json({
+      sucsess: true,
+      message: "signup successed ",
+      data: saveUser,
+    });
+  } catch (e) {
+    return res.json({
+      sucsess: false,
+      message: e.message,
+    });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const loginFind = await User.findOne({ email, password }).select(
+    "name email gender mobile"
+  );
+
+  if (!loginFind) {
+    return res.json({
+      sucsess: false,
+      message: "invalid credentials",
+    });
+  }
+
+  return res.json({
+    sucsess: true,
+    message: "Login successful",
+    data: loginFind,
+  });
+});
 
 app.listen(
   process.env.PORT || 5000,
